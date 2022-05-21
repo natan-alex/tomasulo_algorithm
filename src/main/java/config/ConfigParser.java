2c3e9795c8062323c67cef542f54ed29cdb30ec9
natan-alex <natancarmo321@gmail.com>
2022-05-21 09:41:18 -0300

    add main package with main entrypoint and try read config.json from resources folder

--- src/main/java/config/ConfigParser.java
+++ src/main/java/config/ConfigParser.java
@@ -1,5 +1,16 @@
 package main.java.config;
 
-public class ConfigParser {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
 
+public class ConfigParser {
+    private static final String CONFIG_FILE_PATH = Path.of("main", "resources", "config.json").toString();
+    public ConfigParser() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(CONFIG_FILE_PATH))) {
+            System.out.println(reader.readLine());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main.java.main;
+
+import main.java.config.ConfigParser;
+
+public class Main {
+    public static void main(String[] args) {
+        ConfigParser configParser = new ConfigParser();
+    }
+}
