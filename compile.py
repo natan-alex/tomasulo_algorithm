9f8fbe3561d5779dc3918723b187ea61971eb5ba
natan-alex <natancarmo321@gmail.com>
2022-05-31 21:52:00 -0300

    make code better in compile.py

--- compile.py
+++ compile.py
@@ -5,15 +5,19 @@ import shutil
 def main():
     source_dir = os.path.join('.', 'src')
     output_dir = os.path.join('.', 'target')
+    resources_path = os.path.join(output_dir, 'main', 'resources')
     config_filename = 'config.json'
+    current_config_path = os.path.join('.', config_filename)
+    destination_path = os.path.join(resources_path, config_filename)
+
     java_files = [os.path.join(root, file) for root, _, files in os.walk(source_dir) for file in files if file.endswith('.java')]
-    compile_command = f'javac -d {output_dir} {" ".join(java_files)}'
-    os.system(compile_command)
-    config_file_path = os.path.join('.', config_filename)
-    destination_path = os.path.join(f'{output_dir}', 'main', 'resources', f'{config_filename}')
-    shutil.copyfile(config_file_path, destination_path)
-    run_command = f'cd {output_dir} && java main.java.main.Main'
-    os.system(run_command)
+    os.system(f'javac -d {output_dir} {" ".join(java_files)}')
+
+    if not os.path.exists(resources_path):
+        os.mkdir(resources_path)
+
+    shutil.copyfile(current_config_path, destination_path)
+    os.system(f'cd {output_dir} && java main.java.main.Main')
 
 
 if __name__ == '__main__':
