0b8ddaa716819246b65238172001038bbe4b0a70
natan-alex <natancarmo321@gmail.com>
2022-06-05 10:56:44 -0300

    add operation field to base instruction

--- src/main/java/instructions/Instruction.java
+++ src/main/java/instructions/Instruction.java
@@ -1,24 +1,36 @@
 package main.java.instructions;
 
-import java.util.Optional;
-
 import main.java.instructions.states.State;
 import main.java.instructions.states.ToBeScheduled;
 
 public abstract class Instruction {
     private State state;
-    public final InstructionType type;
 
-    public Instruction(InstructionType type) {
+    public final InstructionType type;
+    public final Operation operation;
+
+    public Instruction(InstructionType type, Operation operation) {
         this.state = new ToBeScheduled();
         this.type = type;
+        this.operation = operation;
+        checkIfInstructionAndOperationMatch();
+    }
+
+    private void checkIfInstructionAndOperationMatch() {
+        if (operation.relatedInstructionType != type) {
+            throw new IllegalArgumentException("Instruction type and operation do not match.");
+        }
     }
 
     public void changeState() {
-        state = state.nextState();
+        var nextState = state.nextState();
+        
+        if (nextState != null) {
+            state = nextState;
+        }
     }
 
-    public Optional<State> getState() {
-        return Optional.ofNullable(state);
+    public State getState() {
+        return state;
     }
 }
--- src/main/java/instructions/RTypeInstruction.java
+++ src/main/java/instructions/RTypeInstruction.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 import main.java.components.registers.FPRegister;
 
 public class RTypeInstruction extends Instruction {
-    public final Operation operation;
     public final FPRegister destination;
     public final FPRegister firstOperand;
     public final FPRegister secondOperand;
@@ -16,19 +15,10 @@ public class RTypeInstruction extends Instruction {
         FPRegister firstOperand,
         FPRegister secondOperand
     ) {
-        super(InstructionType.R);
+        super(InstructionType.R, operation);
 
-        this.operation = requireRTypeInstructionRelatedOperation(operation);
         this.destination = Objects.requireNonNull(destination);
         this.firstOperand = Objects.requireNonNull(firstOperand);
         this.secondOperand = Objects.requireNonNull(secondOperand);
-    }
-
-    private static Operation requireRTypeInstructionRelatedOperation(Operation operation) {
-        if (operation.relatedInstructionType != InstructionType.R) {
-            throw new IllegalArgumentException("Only R type instruction related operations allowed.");
-        }
-
-        return operation;
     }
 }
