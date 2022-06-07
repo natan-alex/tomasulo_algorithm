b17fd1ce8c59c5934c0cac09434823535d03b288
natan-alex <natancarmo321@gmail.com>
2022-06-06 21:15:01 -0300

    make a simple allocation of station in architecture

--- src/main/java/components/Architecture.java
+++ src/main/java/components/Architecture.java
@@ -1,46 +1,26 @@
 package main.java.components;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 import main.java.components.registers.RegistrarBuffer;
-import main.java.components.registers.RegistrarManager;
-import main.java.components.stations.ReservationStation;
+import main.java.components.stations.ReservationStationManager;
 import main.java.config.Config;
-import main.java.instructions.Instruction;
-import main.java.instructions.InstructionType;
-import main.java.instructions.Operation;
 import main.java.instructions.RTypeInstruction;
 
 public class Architecture {
-    private final ReservationStation[] addAndSubStations;
-    private final ReservationStation[] mulAndDivStations;
+    private final ReservationStationManager stationsManager;
     private final InstructionQueue instructionQueue;
-    private final RegistrarManager registrarFile;
+    private final RegistrarBuffer registrarBuffer;
 
     public Architecture(Config config) {
-        var addAndSubOperations = new Operation[] { Operation.ADD, Operation.SUB };
-        var mulAndDivOperations = new Operation[] { Operation.MUL, Operation.DIV };
-
-        addAndSubStations = createAndInitStations(config.numberOfAddStations, addAndSubOperations);
-        mulAndDivStations = createAndInitStations(config.numberOfMulStations, mulAndDivOperations);
-
+        stationsManager = new ReservationStationManager(config.numberOfAddStations, config.numberOfMulStations);
         instructionQueue = new InstructionQueue(config.instructionQueueLength);
 
-        registrarFile = new RegistrarBuffer(config.numberOfFloatingPointRegisters);
+        registrarBuffer = new RegistrarBuffer(config.numberOfFloatingPointRegisters);
+        registrarBuffer.setRandomValuesInRegisters();
     }
 
-    private static final ReservationStation[] createAndInitStations(int numberOfStations, Operation[] operations) {
-        var stations = new ReservationStation[numberOfStations];
-
-        for (int i = 0; i < numberOfStations; i++) {
-            stations[i] = new ReservationStation(operations[0].representation, operations);
-        }
-
-        return stations;
-    }
-
-    public void schedule(Instruction instruction) {
+    public void schedule(RTypeInstruction instruction) {
         Objects.requireNonNull(instruction);
         instructionQueue.enqueue(instruction);
     }
@@ -59,47 +39,29 @@ public class Architecture {
         }
     }
 
-    private ReservationStation[] getCorrespondingStationsFor(Instruction instruction) {
-        if (instruction.operation.isMulOrDiv()) {
-            return mulAndDivStations;
-        }
+    private boolean tryAllocateInANotBusyStation(RTypeInstruction instruction) {
+        var result = stationsManager.getNotBusyStationForOperation(instruction.operation);
 
-        return addAndSubStations;
-    }
-
-    private boolean tryAllocateInANotBusyStation(Instruction instruction) {
-        if (instruction.type == InstructionType.R) {
-            return tryAllocateInANotBusyStationForRTypeInstruction((RTypeInstruction) instruction);
-        }
-
-        return false;
-    }
-
-    private boolean tryAllocateInANotBusyStationForRTypeInstruction(RTypeInstruction instruction) {
-        var stations = getCorrespondingStationsFor(instruction);
-        var optional = Arrays.stream(stations).filter(s -> !s.isBusy).findFirst();
-        
-        if (optional.isEmpty()) {
+        if (result.isEmpty()) {
             System.out.println("All busy");
             return false;
         }
 
-        var station = optional.get();
-        station.isBusy = true;
+        var station = result.get();
 
-        if (registrarFile.isRegisterWaiting(instruction.firstOperand.name)) {
-            var stationName = registrarFile.getStationThatWillProduceValueFor(instruction.firstOperand.name);
+        if (registrarBuffer.isRegisterWaiting(instruction.firstOperand.name)) {
+            var stationName = registrarBuffer.getStationThatWillProduceValueFor(instruction.firstOperand.name);
             station.firstStationThatWillProduceValue = stationName.get();
         } else {
-            var value = registrarFile.getValueFromRegister(instruction.firstOperand.name);
+            var value = registrarBuffer.getValueFromRegister(instruction.firstOperand.name);
             station.firstOperandValue = value.get();
         }
 
-        if (registrarFile.isRegisterWaiting(instruction.secondOperand.name)) {
-            var stationName = registrarFile.getStationThatWillProduceValueFor(instruction.secondOperand.name);
+        if (registrarBuffer.isRegisterWaiting(instruction.secondOperand.name)) {
+            var stationName = registrarBuffer.getStationThatWillProduceValueFor(instruction.secondOperand.name);
             station.secondStationThatWillProduceValue = stationName.get();
         } else {
-            var value = registrarFile.getValueFromRegister(instruction.secondOperand.name);
+            var value = registrarBuffer.getValueFromRegister(instruction.secondOperand.name);
             station.secondOperandValue = value.get();
         }
 
@@ -107,13 +69,6 @@ public class Architecture {
     }
 
     public void showReservationStations() {
-        for (var reservationStation : addAndSubStations) {
-            System.out.println(reservationStation.isBusy);
-            System.out.println(reservationStation.firstOperandValue);
-            System.out.println(reservationStation.firstStationThatWillProduceValue);
-            System.out.println(reservationStation.secondOperandValue);
-            System.out.println(reservationStation.secondStationThatWillProduceValue);
-            System.out.println();
-        }
+        stationsManager.showStations();
     }
 }
