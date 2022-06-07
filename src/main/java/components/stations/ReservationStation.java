9d9ceeba434934ea9fcc0371f1f8ab0eaab33cf5
natan-alex <natancarmo321@gmail.com>
2022-06-06 21:12:53 -0300

    work with simple operation and types in reservation station

--- src/main/java/components/stations/ReservationStation.java
+++ src/main/java/components/stations/ReservationStation.java
@@ -2,25 +2,24 @@ package main.java.components.stations;
 
 import java.util.Objects;
 
-import main.java.components.registers.FPRegister;
 import main.java.instructions.Operation;
 
 public class ReservationStation {
     public final String name;
-    public final Operation[] operations;
+    public final Operation operation;
     public boolean isBusy;
-    public Double firstOperandValue;
-    public Double secondOperandValue;
+    public double firstOperandValue;
+    public double secondOperandValue;
     public String firstStationThatWillProduceValue;
     public String secondStationThatWillProduceValue;
     public Object immediateOrAddress;
 
-    public ReservationStation(String name, Operation[] operations) {
+    public ReservationStation(String name, Operation operation) {
         this.name = Objects.requireNonNull(name);
-        this.operations = Objects.requireNonNull(operations);
+        this.operation = operation;
         this.isBusy = false;
-        this.firstOperandValue = null;
-        this.secondOperandValue = null;
+        this.firstOperandValue = 0.0;
+        this.secondOperandValue = 0.0;
         this.firstStationThatWillProduceValue = null;
         this.secondStationThatWillProduceValue = null;
         this.immediateOrAddress = null;
