916b5c9917cff34ff5d9045ae5a2194f898ec603
natan-alex <natancarmo321@gmail.com>
2022-06-07 19:57:55 -0300

    change registrar buffer to registrar bank

---
+++ src/main/java/components/registers/RegistrarBank.java
@@ -0,0 +1,60 @@
+package main.java.components.registers;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RegistrarBank {
+    public static final String REGISTER_NAME_PREFIX = "F";
+
+    private final FPRegister[] registers;
+
+    public RegistrarBank(int numberOfRegisters) {
+        if (numberOfRegisters <= 0) {
+            throw new IllegalArgumentException("The number of registers must be positive and greather than 0");
+        }
+
+        registers = new FPRegister[numberOfRegisters];
+
+        initRegisters();
+    }
+
+    private void initRegisters() {
+        for (int i = 0; i < registers.length; i++) {
+            registers[i] = new FPRegister(REGISTER_NAME_PREFIX + i);
+        }
+    }
+
+    public void setRandomValuesInRegisters() {
+        var random = new Random();
+
+        for (var register : registers) {
+            register.value = (random.nextDouble() + 5) % 20;
+        }
+    }
+
+    public String[] getRegistrarNames() {
+        return Arrays.stream(registers)
+                .map(r -> r.name)
+                .toArray(String[]::new);
+    }
+
+    private FPRegister getRegisterWithNameOrThrow(String name) {
+        var result = Arrays.stream(registers)
+                .filter(e -> e.name.equalsIgnoreCase(name))
+                .findFirst();
+
+        return result.orElseThrow();
+    }
+
+    public void setValueForRegister(String registerName, double value) {
+        var register = getRegisterWithNameOrThrow(registerName);
+
+        register.value = value;
+    }
+
+    public double getRegisterValue(String registerName) {
+        var register = getRegisterWithNameOrThrow(registerName);
+
+        return register.value;
+    }
+}
