68cd3b8094f516373c87886548ff13a812cf173f
natan-alex <natancarmo321@gmail.com>
2022-06-07 23:24:44 -0300

    a bit of cycles in add station

--- src/main/java/components/units/AddFunctionalUnit.java
+++ src/main/java/components/units/AddFunctionalUnit.java
@@ -1,14 +1,21 @@
 package main.java.components.units;
 
 public class AddFunctionalUnit implements FunctionalUnit {
+    private static final int NUMBER_OF_CYCLES = 2;
     private final Thread thread;
     private double firstOperand;
     private double secondOperand;
 
     public AddFunctionalUnit() {
         this.thread = new Thread(() -> {
+            try {
+                Thread.sleep(NUMBER_OF_CYCLES * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             var result = this.firstOperand + this.secondOperand;
             // send to common data bus
+            System.out.println("from thread in add unit: " + result);
         });
     }
 
