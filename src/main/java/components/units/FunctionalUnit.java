1e1015a74d4e11d49491e7f3b1786ce831df961a
natan-alex <natancarmo321@gmail.com>
2022-06-11 09:34:10 -0300

    remove unnecessary code in functional unit

--- src/main/java/components/units/FunctionalUnit.java
+++ src/main/java/components/units/FunctionalUnit.java
@@ -6,61 +6,46 @@ import main.java.components.busses.DataBus;
 import main.java.instructions.RTypeInstruction;
 
 public abstract class FunctionalUnit {
-    private Thread thread;
-
+    private final String name;
     private DataBus commonDataBus;
     private int cyclesRequiredToPerformOperation;
 
-    public FunctionalUnit(int cyclesRequiredToPerformOperation, DataBus commonDataBus) {
+    public FunctionalUnit(String unitName, int cyclesRequiredToPerformOperation, DataBus commonDataBus) {
         if (cyclesRequiredToPerformOperation <= 0) {
             throw new IllegalArgumentException("Number of cycles must be greather than 0.");
         }
 
+        this.name = Objects.requireNonNull(unitName);
         this.cyclesRequiredToPerformOperation = cyclesRequiredToPerformOperation;
         this.commonDataBus = Objects.requireNonNull(commonDataBus);
     }
 
     public abstract void calculateResultFor(RTypeInstruction instruction);
 
-    protected abstract String getUnitName();
+    public void execute(RTypeInstruction instruction) {
+        Objects.requireNonNull(instruction);
 
-    private void trySleep(int millis) {
+        new Thread(() -> {
+            System.out.println("LOG from " + name + ":");
+            System.out.println("\tCalculating result...");
+
+            trySleep(cyclesRequiredToPerformOperation * 1000);
+            calculateResultFor(instruction);
+
+            var result = instruction.getDestination().getValue().get();
+            System.out.println("LOG from " + name + " unit:");
+            System.out.println("\tSending result << " + result + " >> to common data bus.");
+
+            commonDataBus.notifyObserversWith(instruction);
+            System.out.println("finishing thread " + Thread.currentThread().getName());
+        }).start();
+    }
+
+    private static void trySleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    private void waitThreadIfAlive() {
-        if (thread != null && thread.isAlive()) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void execute(RTypeInstruction instruction) {
-        Objects.requireNonNull(instruction);
-
-        waitThreadIfAlive();
-
-        thread = new Thread(() -> {
-            System.out.println("LOG from " + getUnitName() + ":");
-            System.out.println("\tCalculating result...");
-            trySleep(cyclesRequiredToPerformOperation * 1000);
-
-            calculateResultFor(instruction);
-
-            var result = instruction.getDestination().getValue().get();
-            System.out.println("LOG from " + getUnitName() + ":");
-            System.out.println("\tSending result << " + result + " >> to common data bus.");
-
-            commonDataBus.notifyObserversWith(instruction);
-        });
-
-        thread.run();
     }
 }
