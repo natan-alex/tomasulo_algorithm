07622f7105fc0d7164a4a2dd643bfd443cdfd8f9
natan-alex <natancarmo321@gmail.com>
2022-06-12 23:16:32 -0300

    add class to hold infos to pass to station, unit and common data bus to broadcast

--- src/main/java/components/stations/Station.java
+++ src/main/java/components/stations/Station.java
@@ -1,17 +1,9 @@
 package main.java.components.stations;
 
-import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-
 import main.java.components.busses.BusObserver;
-import main.java.instructions.RTypeInstruction;
 
 public interface Station extends BusObserver {
-    void storeInstructionAndTryDispatch(
-            RTypeInstruction instruction,
-            Optional<String> firstOperandNewName,
-            Optional<String> secondOperandNewName,
-            CountDownLatch countDownLatch);
+    void storeInfosAndTryDispatch(StationStorableInfos info);
     
     boolean isBusy();
 
