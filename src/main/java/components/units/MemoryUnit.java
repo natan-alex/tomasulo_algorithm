e77b19371fc5eea5d05c2a35baec36b189189e4d
natan-alex <natancarmo321@gmail.com>
2022-06-14 22:15:33 -0300

    rename run methods and uppercase name

--- src/main/java/components/units/MemoryUnit.java
+++ src/main/java/components/units/MemoryUnit.java
@@ -1,13 +1,14 @@
 package main.java.components.units;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Random;
 
 import main.java.components.busses.DataBus;
 import main.java.instructions.Operation;
 
 public class MemoryUnit implements BaseMemoryUnit {
-    public static final String NAME = "Memory unit";
+    public static final String NAME = "MEMORY UNIT";
     private final DataBus commonDataBus;
     private final int timeToCalculateResult;
 
@@ -41,40 +42,44 @@ public class MemoryUnit implements BaseMemoryUnit {
 
         thread = new Thread(() -> {
             if (infos.getOperation() == Operation.STORE) {
-                runStoreOperationCode(infos);
+                executeStoreOperationCode(infos);
             } else {
-                runLoadOperationCode(infos);
+                executeLoadOperationCode(infos);
             }
         });
 
         thread.start();
     }
 
-    private void runStoreOperationCode(MemoryUnitBroadcastInfos infos) {
-        System.out.println("LOG from memory unit:"
-                + "\n\tStoring value in memory in address << " + infos.getAddress() + " >>");
+    private void executeStoreOperationCode(MemoryUnitBroadcastInfos infos) {
+        System.out.println("LOG from " + NAME + ":"
+                + "\n\tStoring value << " + infos.getDestinationRegisterValue().get() + " >>"
+                + " in memory address << " + infos.getAddress() + " >>");
 
         trySleep(timeToCalculateResult);
 
-        System.out.println("LOG from memory unit:"
-                + "\n\tValue was stored in memory, telling common data bus");
+        System.out.println("LOG from " + NAME + ":"
+                + "\n\tValue << " + infos.getDestinationRegisterValue().get() + " >>"
+                + " got from << " + infos.getDestinationRegisterName() + " >>"
+                + " was stored in memory, telling common data bus");
 
         infos.getCountDownLatch().countDown();
-        commonDataBus.notifyObserversWith(infos, 0);
+        commonDataBus.notifyObserversWith(infos, Optional.empty());
     }
 
-    private void runLoadOperationCode(MemoryUnitBroadcastInfos infos) {
-        System.out.println("LOG from memory unit:"
-                + "\n\tGetting value from memory in address << " + infos.getAddress() + " >>");
+    private void executeLoadOperationCode(MemoryUnitBroadcastInfos infos) {
+        System.out.println("LOG from " + NAME + ":"
+                + "\n\tGetting value from memory in address << " + infos.getAddress() + " >>"
+                + " to store in register << " + infos.getDestinationRegisterName() + " >>");
 
         var result = getRandomValueInRange(1, 50);
         trySleep(timeToCalculateResult);
 
-        System.out.println("LOG from memory unit:"
+        System.out.println("LOG from " + NAME + ":"
                 + "\n\tSending result << " + result + " >> to common data bus");
 
         infos.getCountDownLatch().countDown();
-        commonDataBus.notifyObserversWith(infos, result);
+        commonDataBus.notifyObserversWith(infos, Optional.of(result));
     }
 
     private double getRandomValueInRange(double min, double max) {
